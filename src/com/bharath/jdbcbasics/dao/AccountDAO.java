c5b7a09a8b5587cc1f7343736a38dfed549c11d2
Andrei Cristea <andreicristea@Andreis-MacBook-Pro.local>
2017-12-20 10:40:58 +0200

    changes

--- src/com/bharath/jdbcbasics/dao/AccountDAO.java
+++ src/com/bharath/jdbcbasics/dao/AccountDAO.java
@@ -1,23 +1,41 @@
 package com.bharath.jdbcbasics.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class AccountDAO {
 
-    public static void main(String[] args) throws SQLException{
+    public static void main(String[] args){
 
-       Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:6603/mydb","root","mypassword");
-       Statement statement = connection.createStatement();
-       int result = statement.executeUpdate("INSERT INTO account VALUES (1,'andrei','cristea','100')");
-        System.out.println(result);
+        try( Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:6603/mydb", "root", "mypassword");
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery("SELECT * FROM account");){
 
-        int resultUpdate = statement.executeUpdate("UPDATE account set bal=500 where accno=1");
-        System.out.println(resultUpdate);
 
-        int deleteResult = statement.executeUpdate("DELETE from account where accno=1");
-        System.out.println(deleteResult);
+            // int result = statement.executeUpdate("INSERT INTO account VALUES (1,'andrei','cristea','100')");
+            // System.out.println(result);
+
+            // int resultUpdate = statement.executeUpdate("UPDATE account set bal=500 where accno=1");
+            // System.out.println(resultUpdate);
+
+            // int deleteResult = statement.executeUpdate("DELETE from account where accno=1");
+            // System.out.println(deleteResult);
+
+
+            while (rs.next()) {
+                System.out.println(
+                        "Account number " +
+                                rs.getString("accno") +
+                                " with name " +
+                                rs.getString("firstname") + " " +
+                                rs.getString("lastname") +
+                                " has balance of " +
+                                rs.getString("bal"));
+            }
+        }
+        catch (SQLException e){
+
+        }
+
+
     }
 }
